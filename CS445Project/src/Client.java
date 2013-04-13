6dd482b1c823e84bfb00b085004e90c3fe12ecd6
Kristen Deppe <kristen.deppe@gmail.com>
2013-04-13 13:32:38 -0500

    Outlined methods for Client and Tour

--- CS445Project/src/Client.java
+++ CS445Project/src/Client.java
@@ -18,5 +18,55 @@ public class Client
     double totalSpent;
     ArrayList<Booking> bookings;
     
+    //Constructor
+    public Client(String n, String e, String p) {
+        name = n;
+        email = e;
+        phone = p;
+        totalSpent = 0;
+        bookings = new ArrayList<Booking>(0);
+    }
     
+    //Accessors
+    String getName() {
+        return name;
+    }
+    
+    String getEmail() {
+        return email;
+    }
+    
+    String getPhone() {
+        return phone;
+    }
+    
+    double getTotalSpent() {
+        return totalSpent;
+    }
+    
+    ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+    
+    //Mutators
+    void setName(String n) {
+        name = n;
+    }
+    
+    void setEmail(String e) {
+        email = e;
+    }
+    
+    void setPhone(String p) {
+        phone = p;
+    }
+    
+    //Actions
+    void makeBooking(Booking b) {
+        
+    }
+    
+    void cancelBooking(Booking b) {
+        
+    }
 }
--- CS445Project/src/Tour.java
+++ CS445Project/src/Tour.java
@@ -17,4 +17,76 @@ public class Tour
     GregorianCalendar end;
     int capacity;
     ArrayList<Booking> bookings;
+    
+    //Constructor
+    public Tour(double p, GregorianCalendar st, GregorianCalendar e, int cap)
+    {
+        price = p;
+        start = st;
+        end = e;
+        capacity = cap;
+        bookings = new ArrayList<Booking>(0);
+    }
+    //Accessors
+    double getPrice() {
+        return price;
+    }
+    
+    GregorianCalendar getStart() {
+        return start;
+    }
+    
+    GregorianCalendar getEnd() {
+        return end;
+    }
+    
+    int getCapacity() {
+        return capacity;
+    }
+    
+    ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+    
+    int getRemaining() {
+        return capacity-bookings.size();
+    }
+    //Mutators
+    
+    void setPrice(double p) {
+        price = p;
+    }
+    
+    void setStart(GregorianCalendar s) {
+        start = s;
+    }
+    
+    void setEnd(GregorianCalendar e) {
+        end = e;
+    }
+    
+    void setCapacity(int cap) {
+        capacity = cap;
+    }
+    
+    //Actions
+    void cancelTour() {
+        
+    }
+    
+    void setPrice(int price) {
+        
+    }
+    
+    void makeBooking(Booking b) {
+        
+    }
+    
+    void cancelBooking(Booking b) {
+        
+    }
+    
+    void listEmails() {
+        
+    }
 }
