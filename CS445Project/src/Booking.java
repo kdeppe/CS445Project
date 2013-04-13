cb4a7f16009772f504983b6b7a6c8d8734f1be96
Kristen Deppe <kristen.deppe@gmail.com>
2013-04-13 14:03:38 -0500

    Booking.java tentatively complete

--- CS445Project/src/Booking.java
+++ CS445Project/src/Booking.java
@@ -11,4 +11,28 @@ public class Booking
 {
     Tour tour;
     Client client;
+    
+    //Constructor
+    public Booking(Tour t, Client c) {
+        tour = t;
+        client = c;
+    }
+    
+    //Accessors
+    Tour getTour() {
+        return tour;
+    }
+    
+    Client getClient() {
+        return client;
+    }
+    
+    //Mutators
+    void setTour(Tour t) {
+        tour = t;
+    }
+    
+    void setClient(Client c) {
+        client = c;
+    }
 }
---
+++ CS445Project/src/TourManager.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kristen
+ */
+import java.util.*;
+
+public class TourManager 
+{
+    public static void main(String [] args) {
+    
+    }
+    
+    void viewTours() {
+        
+    }
+    
+    void changeBooking() {
+        
+    }
+    
+    void displayPastBookings(GregorianCalendar start, GregorianCalendar end) {
+        
+    }
+    
+    void displayRevenue(GregorianCalendar start, GregorianCalendar end) {
+        
+    }
+}
