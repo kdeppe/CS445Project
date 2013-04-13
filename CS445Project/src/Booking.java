0a0cefed0e97d8aff2edbe36651a299c8640a7f0
Kristen Deppe <kristen.deppe@gmail.com>
2013-04-13 12:37:39 -0500

    Initial commit, work started on object classes

---
+++ CS445Project/src/Booking.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kristen
+ */
+public class Booking 
+{
+    Tour tour;
+    Client client;
+}
---
+++ CS445Project/src/Tour.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kristen
+ */
+
+import java.util.*;
+
+public class Tour 
+{
+    double price;
+    GregorianCalendar start;
+    GregorianCalendar end;
+    int capacity;
+    ArrayList<Booking> bookings;
+}
